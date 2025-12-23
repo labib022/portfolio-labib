51bc191a40578e6a7022fb0f3738c29056fe0ac3
Md Labebul Islam <86649900+labib022@users.noreply.github.com>
2025-12-23 22:34:35 +0600

    final Update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -30,7 +30,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name">MD LABEBUL ISLAM</strong>
+                <strong className="main-name"> MD LABEBUL ISLAM</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: 'left' }}>
--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -4,7 +4,14 @@ import { Card, Button, Col } from 'react-bootstrap';
 function ProjectCard({ title, description, techStack, liveLink, githubLink }) {
   return (
     <Col md={6} lg={4} className="project-card">
-      <Card style={{ marginBottom: '30px', minHeight: '300px' }}>
+      <Card
+        style={{
+          marginBottom: '30px',
+          minHeight: '300px',
+          backgroundColor: '#613182',
+          color: 'white',
+        }}
+      >
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Card.Text>{description}</Card.Text>
@@ -12,7 +19,7 @@ function ProjectCard({ title, description, techStack, liveLink, githubLink }) {
             <strong>Tech Stack:</strong> {techStack}
           </p>
 
-          <Button variant="dark" href={githubLink} target="_blank">
+          <Button variant="light" href={githubLink} target="_blank">
             GitHub
           </Button>
         </Card.Body>
